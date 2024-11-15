9654d39e284f30c6997d453a42a61616ccebe8e7
Olivia Madume <olivia.madume@redcloudtechnology.com>
2024-11-15 13:13:35 +0000

    adding db/schema name to script guideline

--- additional_scripts.sql
+++ additional_scripts.sql
@@ -1,53 +1,53 @@
 -- Backup scripts:
 -- For entire tables
-CREATE TABLE backup_customer_entity AS SELECT * FROM customer_entity;
-CREATE TABLE backup_customer_address_entity AS SELECT * FROM customer_address_entity;
+CREATE TABLE <db_name>.backup_customer_entity AS SELECT * FROM <db_name>.customer_entity;
+CREATE TABLE <db_name>.backup_customer_address_entity AS SELECT * FROM <db_name>.customer_address_entity;
 -- For the specific customer records only
-CREATE TABLE backup_customer_entity AS SELECT * FROM customer_entity WHERE entity_id IN (<customer_ids>);
-CREATE TABLE backup_customer_address_entity AS SELECT * FROM customer_address_entity WHERE entity_id IN (<customer_ids>);
+CREATE TABLE <db_name>.backup_customer_entity AS SELECT * FROM <db_name>.customer_entity WHERE entity_id IN (<customer_ids>);
+CREATE TABLE <db_name>.backup_customer_address_entity AS SELECT * FROM <db_name>.customer_address_entity WHERE entity_id IN (<customer_ids>);
 
 -- Validation scripts (after transaction completed successfully):
 SELECT 
     (CASE 
         WHEN (SELECT MD5(GROUP_CONCAT(default_billing, default_shipping ORDER BY entity_id)) 
-              FROM customer_entity) != 
+              FROM <db_name>.customer_entity) != 
              (SELECT MD5(GROUP_CONCAT(default_billing, default_shipping ORDER BY entity_id)) 
-              FROM backup_customer_entity) 
+              FROM <db_name>.backup_customer_entity) 
         THEN 'True' 
         ELSE 'False' 
      END) AS data_changed;
 
 SELECT 
     (CASE 
-        WHEN (SELECT COUNT(*) FROM customer_address_entity) > (SELECT COUNT(*) FROM backup_customer_address_entity) 
+        WHEN (SELECT COUNT(*) FROM <db_name>.customer_address_entity) > (SELECT COUNT(*) FROM <db_name>.backup_customer_address_entity) 
         THEN 'True' 
         ELSE 'False' 
      END) AS new_addresses_added;
 
 -- Rollback scripts:
 -- Reset default billing and shipping values to previous
-UPDATE customer_entity
-JOIN backup_customer_entity ON customer_entity.entity_id = backup_customer_entity.entity_id
-SET customer_entity.default_billing = backup_customer_entity.default_billing,
-    customer_entity.default_shipping = backup_customer_entity.default_shipping
-WHERE customer_entity.entity_id IN (<customer_ids>);
+UPDATE <db_name>.customer_entity
+JOIN <db_name>.backup_customer_entity ON <db_name>.customer_entity.entity_id = <db_name>.backup_customer_entity.entity_id
+SET <db_name>.customer_entity.default_billing = <db_name>.backup_customer_entity.default_billing,
+    <db_name>.customer_entity.default_shipping = <db_name>.backup_customer_entity.default_shipping
+WHERE <db_name>.customer_entity.entity_id IN (<customer_ids>);
 -- Delete newly-added address records
 SELECT GROUP_CONCAT(address_entity_id SEPARATOR ',') AS address_ids_to_export FROM <db_name>.new_address_ids; -- Then export as a csv and use for delete below!
-DELETE FROM customer_address_entity WHERE entity_id IN (<new_address_record_ids>);
+DELETE FROM <db_name>.customer_address_entity WHERE entity_id IN (<new_address_record_ids>);
 -- Validate rollbacks were successful
 SELECT 
     (CASE 
         WHEN (SELECT MD5(GROUP_CONCAT(default_billing, default_shipping ORDER BY entity_id)) 
-              FROM customer_entity) = 
+              FROM <db_name>.customer_entity) = 
              (SELECT MD5(GROUP_CONCAT(default_billing, default_shipping ORDER BY entity_id)) 
-              FROM backup_customer_entity) 
+              FROM <db_name>.backup_customer_entity) 
         THEN 'True' 
         ELSE 'False' 
      END) AS checksums_match;
 
 SELECT 
     (CASE 
-        WHEN (SELECT COUNT(*) FROM customer_address_entity) = (SELECT COUNT(*) FROM backup_customer_address_entity) 
+        WHEN (SELECT COUNT(*) FROM <db_name>.customer_address_entity) = (SELECT COUNT(*) FROM <db_name>.backup_customer_address_entity) 
         THEN 'True' 
         ELSE 'False' 
      END) AS counts_match;
